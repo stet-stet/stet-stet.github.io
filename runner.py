ae6f69ec7f8c05452cea1055b6dbd38e106f6364
stet-stet <stalemate126@gmail.com>
2019-10-27 23:31:57 +0000

    update

--- runner.py
+++ runner.py
@@ -2,10 +2,10 @@ import subprocess
 from time import sleep
 
 def doit():
-  subprocess.call("git pull origin master",shell=True)
   subprocess.call("node scrap.js",shell=True)
   subprocess.call("git add *",shell=True)
   subprocess.call("git commit -m 'update'",shell=True)
+  subprocess.call("git pull origin master",shell=True)
   subprocess.call("git push origin master",shell=True)
 
 if __name__=="__main__":
